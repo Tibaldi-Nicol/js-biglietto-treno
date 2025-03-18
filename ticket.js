58439bdcf1051328cb2af9f2cecc4653180034c4
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-03-18 09:36:29 +0100

    [UPD]ADD-alert

---
+++ ticket.js
@@ -0,0 +1,9 @@
+// Chiediamo all'utente i chilometri da percorrere
+const chilometri = prompt("Inserisci il numero di chilometri:");
+
+// Chiediamo all'utente l'età del passeggero
+const eta = prompt("Inserisci l'età del passeggero:");
+
+// Calcoliamo il prezzo base: 0.21 € per km
+const prezzoBase = chilometri * 0.21;
+
