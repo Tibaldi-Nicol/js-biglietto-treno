61d9613ecbaa2615f92bf7fd8e40356efe22e72d
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-03-18 09:38:58 +0100

    [UPD]eta<18

--- ticket.js
+++ ticket.js
@@ -6,4 +6,16 @@ const eta = prompt("Inserisci l'età del passeggero:");
 
 // Calcoliamo il prezzo base: 0.21 € per km
 const prezzoBase = chilometri * 0.21;
+// Definiamo una variabile per il prezzo finale
+let prezzoFinale;
 
+// sconti in base all'età
+if (eta < 18) {
+    // Sconto del 20% per i minor
+
+
+    prezzoFinale = prezzoBase - (prezzoBase * 0.2);
+
+
+} else if (eta >= 65) {
+    
